93c68623c7cf37ab20243914d13c7f39c20772a0
hazmansnorlaxhassan <hazmanhassan912@gmail.com>
2014-11-15 03:13:43 +0800

    Update show.php

--- show.php
+++ show.php
@@ -12,10 +12,7 @@
 		$result = mysql_query($sql) or die ("Query error: " . mysql_error());
 	
 		while($row = mysql_fetch_array($result)) {
-			echo "<link href='http://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.3/jquery.mobile.min.css' rel='stylesheet' type='text/css'/>";
-			echo "<script src='http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js' type='text/javascript'></script>";
-			echo "<script src='http://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.3/jquery.mobile.min.js' type='text/javascript'></script>";
-			echo "<div data-role='collapsible data-content-theme='false'><h4>Title: ".$row['post_title']."</h4><p>".$row['post_content']."</p><p>".$row['post_date']."</p></div>";
+			echo "<h4>Title: ".$row['post_title']."</h4><p>".$row['post_content']."</p><p>".$row['post_date']."</p>";
 		}
 
 		mysql_close($con);
