44ecea69e9e6a0ee0ffdf95eaf127e634b99ec5d
status90 <juan.status90@live.com>
2014-11-16 20:10:36 +0800

    Update save.php

--- save.php
+++ save.php
@@ -1,25 +1,25 @@
 <?php
-    try {
+try {
 	$server = "mysql2.000webhost.com";
 	$username = "a6422285_itbina";
 	$password = "itbina123";
-    	$database = "a6422285_itbina";
+	$database = "a6422285_itbina";
 
-    	$con = mysql_connect($server, $username, $password) or die ("Could not connect: " . mysql_error());
+	$con = mysql_connect($server, $username, $password) or die ("Could not connect: " . mysql_error());
 	
-    	mysql_select_db($database, $con);
+	mysql_select_db($database, $con);
 	
-    	$title = mysql_real_escape_string($_POST["title"]);
-    	$comment = mysql_real_escape_string($_POST["comment"]);
+	$title = mysql_real_escape_string($_POST["title"]);
+	$comment = mysql_real_escape_string($_POST["comment"]);
 	
-    	mysql_query("INSERT INTO posts (post_title, post_content) VALUES ('$title', '$comment')");
+	mysql_query("INSERT INTO posts (post_title, post_content) VALUES ('$title', '$comment')");
 	
-    	mysql_close($con);
-    	echo "success";
+	mysql_close($con);
+	echo "success";
 	}
-    catch(Exception $e)
+catch(Exception $e)
 	{
-    	echo $e->getMessage();
+	echo $e->getMessage();
 	}
 	
 ?>
